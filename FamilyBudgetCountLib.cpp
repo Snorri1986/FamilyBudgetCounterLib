35bcb8448fc732489d7c621dc834a67d43dfd5c3
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-03-18 20:11:18 +0100

    #1 1 and 2 task. main() initialization

--- FamilyBudgetCountLib.cpp
+++ FamilyBudgetCountLib.cpp
@@ -2,7 +2,11 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Welcome to FamilyBudgetCounter Library" <<std::endl;
+    std::cout << "In a few minutes I will count all your expances and you can see result in Java application" << std::endl;
+    std::cout << "It was a pleasure to work for you!See you later!" << std::endl;
+    //TODO: add list of methods
+    system("pause>0");
 }
 
 
