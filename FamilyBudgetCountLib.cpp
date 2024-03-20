099124ee00def2b5f552bee774c0c7f7a0fd7db0
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-03-20 19:54:21 +0100

    #1 minor style change

--- FamilyBudgetCountLib.cpp
+++ FamilyBudgetCountLib.cpp
@@ -5,8 +5,10 @@ int main()
     std::cout << "Welcome to FamilyBudgetCounter Library" <<std::endl;
     std::cout << "In a few minutes I will count all your expances and you can see result in Java application" << std::endl;
     std::cout << "It was a pleasure to work for you!See you later!" << std::endl;
-    //TODO: add list of methods
-    system("pause>0");
+    
+   system("pause>0");
 }
+
+//TODO: add list of methods
 
 
