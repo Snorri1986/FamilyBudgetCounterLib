228629e91966c3850fcede82fb4c85985abec9b9
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-03-21 20:40:02 +0100

    #3 method to handle incomes is ready

--- FamilyBudgetCountLib.cpp
+++ FamilyBudgetCountLib.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <cmath> 
 
 // common tests signatures
 int showGreetingMessage();
+char * getIncomeGrapthics(double *incomeValues);
 // unit tests signatures
 void testShowGreetingMessage();
+
+const int dividerDKK = 100;
 
 void main()
 {
@@ -22,6 +26,21 @@ int showGreetingMessage() {
     std::cout << "In a few minutes I will count all your expances and you can see result in Java application" << std::endl;
     std::cout << "It was a pleasure to work for you!See you later!" << std::endl;
     return 0;
+}
+
+// get array of X for incomes
+char * getIncomeGrapthics(double *incomeValues) {
+    int arrLength = sizeof(incomeValues);
+    double sum = 0;
+    for (int i = 0;i <= arrLength;i++) {
+        sum += incomeValues[i];
+    }
+    int countX = static_cast<int>(std::round(sum/dividerDKK));
+    char* arrayIncomeX = new char[countX];
+    for (int i = 0; i <= sizeof(arrayIncomeX);i++) {
+        arrayIncomeX[i] = 'X';
+    }
+    return arrayIncomeX;
 }
 
 //TODO: add list of methods related with library
