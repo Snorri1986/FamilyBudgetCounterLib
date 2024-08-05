468791b770370f74bd9bc2d02a642d0b51bd27af
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-08-05 20:59:26 +0200

    #15 - curl test

--- testEndpoints.ps1
+++ testEndpoints.ps1
@@ -8,3 +8,13 @@ echo ------------------------------------------
 echo http://localhost:8080/v1/income/-11
 curl -v http://localhost:8080/v1/income/-11
 echo ------------------------------------------
+# v1/groceries/<double>
+echo curl -v http://localhost:8080/v1/groceries/123.3
+curl -v http://localhost:8080/v1/groceries/123.3
+echo -------------------------------------------
+echo curl -v http://localhost:8080/v1/groceries/0
+curl -v http://localhost:8080/v1/groceries/0
+echo ------------------------------------------
+echo http://localhost:8080/v1/groceries/-11
+curl -v http://localhost:8080/v1/groceries/-11
+echo ------------------------------------------
