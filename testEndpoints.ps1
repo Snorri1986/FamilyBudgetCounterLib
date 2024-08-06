1788ece88e9e0bc131fee5127558417fa2471f16
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-08-06 22:37:41 +0200

    #17 - test case inside curl. No more test with 0

--- testEndpoints.ps1
+++ testEndpoints.ps1
@@ -2,9 +2,6 @@
 echo curl -v http://localhost:8080/v1/income/90.3
 curl -v http://localhost:8080/v1/income/90.3
 echo -------------------------------------------
-echo curl -v http://localhost:8080/v1/income/0
-curl -v http://localhost:8080/v1/income/0
-echo ------------------------------------------
 echo http://localhost:8080/v1/income/-11
 curl -v http://localhost:8080/v1/income/-11
 echo ------------------------------------------
@@ -12,9 +9,12 @@ echo ------------------------------------------
 echo curl -v http://localhost:8080/v1/groceries/123.3
 curl -v http://localhost:8080/v1/groceries/123.3
 echo -------------------------------------------
-echo curl -v http://localhost:8080/v1/groceries/0
-curl -v http://localhost:8080/v1/groceries/0
-echo ------------------------------------------
 echo http://localhost:8080/v1/groceries/-11
 curl -v http://localhost:8080/v1/groceries/-11
 echo ------------------------------------------
+# v1/housing/<double>
+echo curl -v http://localhost:8080/v1/housing/123.3
+curl -v http://localhost:8080/v1/housing/123.3
+echo -------------------------------------------
+echo http://localhost:8080/v1/housing/-11
+curl -v http://localhost:8080/v1/housing/-11
