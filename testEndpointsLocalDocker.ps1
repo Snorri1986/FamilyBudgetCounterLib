77d5683980aadeef6a7eed672b5ed577af5d9684
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-10-24 22:23:08 +0200

    #29 - added file to test endpoint in Local Docker

---
+++ testEndpointsLocalDocker.ps1
@@ -0,0 +1,65 @@
+# v1/income/<double>
+echo curl -v http://localhost:8080/v1/income/90.3
+curl -v http://localhost:8080/v1/income/90.3
+echo -------------------------------------------
+echo curl -v http://localhost:8080/v1/income/0
+curl -v http://localhost:8080/v1/income/0
+echo -------------------------------------------
+echo http://localhost:8080/v1/income/-11
+curl -v http://localhost:8080/v1/income/-11
+echo ------------------------------------------
+# v1/groceries/<double>
+echo curl -v http://localhost:8080/v1/groceries/123.3
+curl -v http://localhost:8080/v1/groceries/123.3
+echo -------------------------------------------
+echo curl -v http://localhost:8080/v1/groceries/0
+curl -v http://localhost:8080/v1/groceries/0
+echo -------------------------------------------
+echo http://localhost:8080/v1/groceries/-11
+curl -v http://localhost:8080/v1/groceries/-11
+echo ------------------------------------------
+# v1/housing/<double>
+echo curl -v http://localhost:8080/v1/housing/123.3
+curl -v http://localhost:8080/v1/housing/123.3
+echo -------------------------------------------
+echo curl -v http://localhost:8080/v1/housing/0
+curl -v http://localhost:8080/v1/housing/0
+echo -------------------------------------------
+echo http://localhost:8080/v1/housing/-11
+curl -v http://localhost:8080/v1/housing/-11
+# v1/travel/<double>
+echo curl -v http://localhost:8080/v1/travel/123.3
+curl -v http://localhost:8080/v1/travel/123.3
+echo -------------------------------------------
+echo curl -v http://localhost:8080/v1/travel/0
+curl -v http://localhost:8080/v1/travel/0
+echo -------------------------------------------
+echo http://localhost:8080/v1/travel/-11
+curl -v http://localhost:8080/v1/travel/-11
+# v1/entertainment/<double>
+echo curl -v http://localhost:8080/v1/entertainment/123.3
+curl -v http://localhost:8080/v1/entertainment/123.3
+echo -------------------------------------------
+echo curl -v http://localhost:8080/v1/entertainment/0
+curl -v http://localhost:8080/v1/entertainment/0
+echo -------------------------------------------
+echo http://localhost:8080/v1/entertainment/-11
+curl -v http://localhost:8080/v1/entertainment/-11
+# v1/health/<double>
+echo curl -v http://localhost:8080/v1/health/123.3
+curl -v http://localhost:8080/v1/health/123.3
+echo -------------------------------------------
+echo curl -v http://localhost:8080/v1/health/0
+curl -v http://localhost:8080/v1/health/0
+echo -------------------------------------------
+echo http://localhost:8080/v1/health/-11
+curl -v http://localhost:8080/v1/health/-11
+# v1/telecom/<double>
+echo curl -v http://localhost:8080/v1/telecom/123.3
+curl -v http://localhost:8080/v1/telecom/123.3
+echo -------------------------------------------
+echo curl -v http://localhost:8080/v1/telecom/0
+curl -v http://localhost:8080/v1/telecom/0
+echo -------------------------------------------
+echo http://localhost:8080/v1/telecom/-11
+curl -v http://localhost:8080/v1/telecom/-11
